6e50e6145f980d5288d806899089683a24b95a31
Raul Alzate <raulest50@gmail.com>
2019-12-13 19:37:16 -0500

    se logro implementar el primer ejemplo de conexion en socket.io, cada que se ingresa al servidor local, este imprime en el log usuario conectado. es muy importante notar en el index que se importa un script en /socket.io/socket.io.js. este no corresponde a un archivo puesto manualmente en esta ruta, es la misma libreria que en el proceso de inicializacion crea esta ruta. es muy importante al final de index.js usar htt.listen... en lugar de app.listen...

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+
+// para el web server express 
+//var express = require('express');
+//var app = express();
+
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 
+
+// se sirve el index
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000);
+
+
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+  console.log('A user connected');
+
+  //Whenever someone disconnects this piece of code executed
+  socket.on('disconnect', function () {
+     console.log('A user disconnected');
+  });
+});
+
+
+// se activa express server
+http.listen(3000, function(){
+  console.log('listening at port 3000 :)');
+});
+
+// link para instalar board support para nodeMCU v3
+//http://arduino.esp8266.com/stable/package_esp8266com_index.json
